e3c0577b79d02e6e1bb73cc749cd470bb4ef7c6d
Rodney-O-C-Melby <rodmelby@outlook.com>
2022-04-26 22:19:01 +0100

    Update version

--- src/dns-zone-transfer-test/__init__.py
+++ src/dns-zone-transfer-test/__init__.py
@@ -4,6 +4,6 @@ dns zone transfer test init
 A python library of variables auto imported when calling dns-zone-transfer inside a python interpreter.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Rodney O C Melby"
 __credits__ = "Bob Halley"
